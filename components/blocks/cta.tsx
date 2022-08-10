a85ff6df3db0b0012d05f891262b68ecdd967fbd
Tom Benbow <tom@hellomoon.net>
2022-08-10 14:57:52 -0500

    Join us touchups

--- components/blocks/cta.tsx
+++ components/blocks/cta.tsx
@@ -13,8 +13,8 @@ export const CTA = ({ data, parentField = "" }) => {
         className="absolute inset-0 -z-1 bg-white"
         style={{ clipPath: "polygon(7% 7%, 100% 0%, 100% 100%, 7% 93%)" }}
       ></div>
-      <div className={`relative flex items-center gap-10 p-20 px-40 mb-20 max-w-desktop-full mx-auto`}>
-        <div className="content-wrap w-1/3 flex-1">
+      <div className={`relative flex items-center gap-10 p-20 px-40 mb-20 max-w-desktop-full mx-auto sm:flex-col sm:px-0`}>
+        <div className="content-wrap w-1/3 flex-1 max-w-sm sm:order-2 sm:w-full">
           <Content
             label = {data.label}
             headline = {data.headline}
@@ -29,13 +29,13 @@ export const CTA = ({ data, parentField = "" }) => {
             order = {data.style?.contentOrder}
             width = "sm:w-full w-full"
             parentField = {parentField}
-            className = ""
+            className = "text-center"
           />
         </div>
-        <div className="image-wrap flex-none">
+        <div className="image-wrap flex-none sm:order-1 sm:w-full">
           {data.image?.src && (
             <img
-              className="image"
+              className="image sm:mx-auto"
               alt={data.image?.alt}
               src={data.image?.src}
               width="225px"
@@ -44,7 +44,7 @@ export const CTA = ({ data, parentField = "" }) => {
             />
           )}
         </div>
-        <div className="button-wrap w-1/3 flex-1 text-center">
+        <div className="button-wrap w-1/3 flex-1 text-center sm:order-3 sm:w-full">
           <Buttons
             buttons={data.buttons}
             className={``}
