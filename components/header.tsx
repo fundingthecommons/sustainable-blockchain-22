8a1524c61bac3fd32d99ac0230b413a9dd7faa67
Tom Benbow <tom@hellomoon.net>
2022-08-18 17:16:13 -0500

    Add mobile sub nav

--- components/header.tsx
+++ components/header.tsx
@@ -3,7 +3,7 @@ import * as ga from '../lib/ga'
 import { isString, lowerDash } from "../helpers/utilities";
 import { Burger } from "./burger"
 
-const navList = (blocks) => {
+const pageJumps = (blocks) => {
   const anchorLinks: [] = blocks?.filter(block => block.navigationLabel).map(block => block.navigationLabel);
   return anchorLinks;
 }
@@ -33,8 +33,8 @@ const Dropdown = ({item}) => {
     <div className="relative inline-block" ref={ref}>
       <a className="block no-underline cursor-pointer select-none" onClick={() => setIsOpen(!isOpen)}>
         {item.label}
-        <svg className="relative inline-block w-4 h-4 ml-2 -top-px" fill="#ffffff" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
-          <path d="M310.6 246.6l-127.1 128C176.4 380.9 168.2 384 160 384s-16.38-3.125-22.63-9.375l-127.1-128C.2244 237.5-2.516 223.7 2.438 211.8S19.07 192 32 192h255.1c12.94 0 24.62 7.781 29.58 19.75S319.8 237.5 310.6 246.6z"/>
+        <svg className="relative inline-block w-4 h-4 ml-2 -top-px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+          <path fill="#ffffff" d="M192 384c-8.188 0-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L192 306.8l137.4-137.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-160 160C208.4 380.9 200.2 384 192 384z"/>
         </svg>
       </a>
       {isOpen && (
@@ -51,6 +51,42 @@ const Dropdown = ({item}) => {
         </div>
       )}
     </div>
+  );
+}
+
+const MobileSub = ({item, className}) => {
+  const ref = useRef(null);
+  const [ isOpen, setIsOpen] = useState(false);
+  return (
+    <li>
+      <div className={`w-full bg-white h-px opacity-25`} />
+      <a className="block leading-8 py-4 no-underline cursor-pointer select-none" onClick={() => setIsOpen(true)}>
+        {item.label}
+        <svg className="relative inline-block w-3 h-3 ml-1.5 -top-px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+          <path fill="#ffffff" d="M96 480c-8.188 0-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L242.8 256L73.38 86.63c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l192 192c12.5 12.5 12.5 32.75 0 45.25l-192 192C112.4 476.9 104.2 480 96 480z"/>
+        </svg>
+      </a>
+      {/* {isOpen && ( */}
+        <div className={`${className} fixed inset-0 transition-all transform ${isOpen ? 'translate-x-0 opacity-100' : 'translate-x-20 opacity-0 pointer-events-none'}`} >
+          <a className="block p-4 leading-8 mb-1" onClick={() => setIsOpen(false)}>
+            <svg className="inline-block w-3 h-3 mr-1 relative -top-px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+              <path fill="#ffffff" d="M447.1 256C447.1 273.7 433.7 288 416 288H109.3l105.4 105.4c12.5 12.5 12.5 32.75 0 45.25C208.4 444.9 200.2 448 192 448s-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25l160-160c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25L109.3 224H416C433.7 224 447.1 238.3 447.1 256z"/>
+            </svg>
+            Back
+          </a>
+          <ul className="px-5">
+            {item.subNavItems.map(function (subItem, index) {
+              return (
+                <li className="block" key={index}>
+                  <div className={`bg-white h-px opacity-25`} />
+                  <a className={"block no-underline whitespace-nowrap leading-8 py-4"} href={subItem.link} target={linkTarget(subItem.link)}>{subItem.label}</a>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+      {/* )} */}
+    </li>
   );
 }
 
@@ -107,71 +143,84 @@ export const Header = ({
   
   return (
     <section className="relative">
-      <div className={`${sectionClasses} sm:hidden sm:h-screen absolute z-40 top-0 left-0 right-0`}>
-        <div style={backgroundStyles} className={`${backgroundClasses} ${nav.navBackgroundColor} transition duration-400 absolute inset-0 -z-1 hidden sm:block`}></div>
-        <div className={`max-w-desktop-full mx-auto ${nav?.padding}`}>
-          
-          {/* Desktop Nav */}
-          <div className="flex items-center sm:hidden">
-            <Logo className="flex-none" />
-            <ul style={navStyles} className={`${nav.navTypeStyle} ${nav.navAlignment} flex-grow list-none sm:hidden`}>
+      <>
+        {/* Desktop Nav */}
+        <div className="sm:hidden absolute z-10 top-0 left-0 right-0">
+          <div className={`max-w-desktop-full mx-auto ${nav?.padding}`}>
+            <div className="flex items-center">
+              <Logo className="flex-none" />
+              <ul style={navStyles} className={`${nav.navTypeStyle} ${nav.navAlignment} flex-grow list-none sm:hidden`}>
 
-              {/* Page Jumps */}
-              {navList(blocks)?.map(function (item, index) {
+                {pageJumps(blocks)?.map(function (item, index) {
+                  return (
+                    <li className="inline-block ml-6 first:ml-0" key={index}>
+                      <a className={"block no-underline"} href={`#${lowerDash(item)}`} onClick={ () => pageJump(item) }>{item}</a>
+                    </li>
+                  )
+                })}
+
+                {navItems.map(function (item, index) {
+                  const subNavItems = item.subNavItems || null
+                  if (subNavItems) {
+                    return (
+                      <li className="relative inline-block ml-6">
+                        <Dropdown item={item} />
+                      </li>
+                    )
+                  } else {
+                    return (
+                      <li className="inline-block ml-8" key={index}>
+                        <a style={linkStyles} className={"block no-underline"} href={item.link} target={linkTarget(item.link)}>{item.label}</a>
+                      </li>
+                    )
+                  }
+                })}
+
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        {/* Mobile Nav */}
+        <div className={`${sectionClasses} hidden sm:block h-screen fixed z-40 top-0 left-0 right-0`}>
+          <div style={backgroundStyles} className={`${backgroundClasses} ${nav.navBackgroundColor} transition duration-400 absolute inset-0 -z-1 hidden sm:block`}></div>
+          <div className={`w-full ${nav?.padding}`}>
+            <Logo className="absolute top-4 left-4 hidden" />
+            <div className={`absolute top-3 -right-2 py-1 pl-2 pr-6 rounded ${nav.navBackgroundColor}`} onClick={() => setNavOpen(!navOpen)}>
+              <Burger color="white" isOpen={navOpen}  />
+            </div>
+            <ul style={navStyles} className={`${navClasses} ${nav.navTypeStyle} flex-grow hidden sm:block mt-12`}>
+
+              {pageJumps(blocks)?.map(function (item, index) {
                 return (
-                  <li className="inline-block ml-8 first:ml-0" key={index}>
-                    <a className={"block no-underline"} href={`#${lowerDash(item)}`} onClick={ () => pageJump(item) }>{item}</a>
+                  <li className="block" key={index}>
+                    <div className={`bg-white h-px opacity-25`} />
+                    <a style={linkStyles} className={"block no-underline"} href={`#${lowerDash(item)}`} onClick={ () => pageJump(item) }>{item}</a>
                   </li>
                 )
               })}
 
-              {/* Nav Items */}
-              {navItems && navItems.map(function (item, index) {
+              {navItems.map(function (item, index) {
                 const subNavItems = item.subNavItems || null
-
                 if (subNavItems) {
                   return (
-                    <li className="relative inline-block ml-8">
-                      <Dropdown item={item} />
-                    </li>
+                    <MobileSub item={item} className={nav.navBackgroundColor} />
                   )
                 } else {
                   return (
-                    <li className="inline-block ml-8" key={index}>
-                      <a style={linkStyles} className={"block no-underline"} href={item.link} target={linkTarget(item.link)} >{item.label}</a>
+                    <li className="block" key={index}>
+                      <div className={`bg-white h-px opacity-25`} />
+                      <a style={linkStyles} className={"block no-underline"} href={item.link} target={linkTarget(item.link)} onClick={() => setNavOpen(!navOpen)}>{item.label}</a>
                     </li>
                   )
                 }
               })}
+
             </ul>
           </div>
-
-          {/* Mobile Nav */}
-          <Logo className={`absolute top-4 left-4 hidden`} />
-          <div className="absolute top-0 p-4 right-0 hidden" onClick={() => setNavOpen(!navOpen)}>
-            <Burger color="white" isOpen={navOpen}  />
-          </div>
-          <ul style={navStyles} className={`${navClasses} ${nav.navTypeStyle} ${nav.navAlignment} flex-grow list-none hidden sm:block mt-16`}>
-            {navList(blocks)?.map(function (item, index) {
-              return (
-                <li className="block" key={index}>
-                  <div className={`bg-white h-px opacity-25`} />
-                  <a style={linkStyles} className={"block no-underline"} href={`#${lowerDash(item)}`} onClick={ () => pageJump(item) }>{item}</a>
-                </li>
-              )
-            })}
-            {navItems && navItems.map(function (item, index) {
-              return (
-                <li className="block" key={index}>
-                  <div className={`bg-white h-px opacity-25`} />
-                  <a style={linkStyles} className={"block no-underline"} href={item.link} target={linkTarget(item.link)} onClick={() => setNavOpen(!navOpen)}>{item.label}</a>
-                </li>
-              )
-            })}
-          </ul>
-
+          
         </div>
-      </div>
+      </>
     </section>
   );
 };
